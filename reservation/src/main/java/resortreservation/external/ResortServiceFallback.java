981b9853c449aed483ce30813ef4af321aec7b28
simair77 <85722851+simair77@users.noreply.github.com>
2021-07-13 08:41:37 +0900

    Add files via upload
    ResortServiceFallback 추가

---
+++ reservation/src/main/java/resortreservation/external/ResortServiceFallback.java
@@ -0,0 +1,15 @@
+package resortreservation.external;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ResortServiceFallback implements ResortService {
+
+    @Override
+    public Resort getResortStatus(Long id) {
+        System.out.println("Circuit breaker has been opened. Fallback returned instead.");
+        return null;
+    }
+
+}
